f93212df5a305db154367d8391e9ccd01de9cd7d
Githaka <githakamaru@gmail.com>
2021-03-02 12:14:43 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import React from 'react';
+import { Homepage } from "./containers/homepage"
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>App</h1>
+//     </div>
+//   );
+// }
+
+function App(props) {
+  return <Homepage />
+}
+
+export default App;
---
+++ src/containers/homepage/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Homepage(props) {
+    return <div> jambo from Merit Ventures</div>
+
+
+}
