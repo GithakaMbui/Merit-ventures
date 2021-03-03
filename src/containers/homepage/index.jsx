dfca4dda295b37753328c28154b9c2f64547be5d
Githaka <githakamaru@gmail.com>
2021-03-03 15:21:11 +0300

    created a logo section

--- src/containers/homepage/index.jsx
+++ src/containers/homepage/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import styled from "styled-components"
+import { TopSection } from "./topSection"
+
+const PageContainer = styled.div`
+width: 100%;
+height: 100%
+flex-direction: column;
+`; 
 
 export function Homepage(props) {
-    return <div> jambo from Merit Ventures</div>
-
+    //return <div> jambo from Merit Ventures</div>
+    return <PageContainer>
+        <TopSection />
+    </PageContainer>
 
 }
