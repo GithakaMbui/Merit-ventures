4786197a781f5b57ddc46ed1726918503c409423
Githaka <githakamaru@gmail.com>
2021-03-03 16:54:37 +0300

    logo section completed

--- src/containers/homepage/topSection.jsx
+++ src/containers/homepage/topSection.jsx
@@ -5,12 +5,14 @@ import Backgroundimg from "../../assets/pictures/company_team.jpg"
 
 import { Logo } from "../../componets/logo/index"
 import { Marginer } from "../../componets/marginer/index"
-
+import { Button } from "../../componets/button/index"
+import { DownArrow } from "../../componets/downarrow/index"
 const TopContainer = styled.div` 
 width: 100%;
 height: 100vh;
 padding: 0;
 background-image: url(${Backgroundimg});
+position: relative;
 `; 
 
 const BackgroundFilter = styled.div`
@@ -23,10 +25,18 @@ const BackgroundFilter = styled.div`
     `;
 
 const MotivationalText = styled.h1`
-font-size: 33px;
-font-weight: 500;
+font-size: 34px;
+font-weight: bold;
+line-height: 1.4;
 color: #fff;
 margin: 0;
+`;
+
+const DownArrowContainer = styled.div`
+    position: absolute;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
 `;
 
     export function TopSection(props) {
@@ -35,9 +45,14 @@ margin: 0;
     <BackgroundFilter>
     <Marginer direction="vertical" margin="5em" />
     <Logo />
-    <Marginer direction="vertical" margin="2em" />
+    <Marginer direction="vertical" margin="4em" />
     <MotivationalText>Asset Tagging </MotivationalText>
     <MotivationalText>From the Best in the Industry </MotivationalText>
+    <Marginer direction="vertical" margin="2em" />
+    <Button> Start your project</Button>
+    <DownArrowContainer>
+        <DownArrow />
+    </DownArrowContainer>
     </BackgroundFilter>
     </TopContainer> );
 
